a04b476d23a97f6f951d57528f4b51d2237e6050
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-05-11 22:31:54 +0530

    displayed data on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Box,
 	Card,
@@ -11,32 +11,73 @@ import {
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import image from "../assets/1292402.jpg";
+import axios from "axios";
 
 const Cards = () => {
+	const [data, setData] = useState([]);
+
+	const getUserData = async () => {
+		await axios
+			.get("https://randomuser.me/api/?results=50")
+			.then((res) => setData(res.data.results))
+			.catch((err) => console.log(err));
+	};
+
+	useEffect(() => {
+		getUserData();
+	}, []);
+
 	return (
-		<Box
-			sx={{
-				display: "flex",
-				width: "100%",
-				alignItems: "center",
-				justifyContent: "space-evenly"
-			}}>
-			<Card sx={{ maxWidth: 345 }}>
-				<CardActionArea>
-					<CardMedia component="img" height="140" image={image} />
-					<CardContent sx={{ display: "flex", justifyContent: "center" }}>
-						<Typography variant="h6" component="div">
-							Lizard
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions sx={{ display: "flex", justifyContent: "end" }}>
-					<Button size="small">
-						<DeleteIcon />
-					</Button>
-				</CardActions>
-			</Card>
-		</Box>
+		<>
+			<Typography variant="h6">Showing {data.length} data</Typography>
+
+			<Box
+				sx={{
+					display: "flex",
+					width: "100%",
+					alignItems: "center",
+					flexWrap: "wrap",
+					justifyContent: "center",
+					gap: "20px",
+					marginTop: "50px"
+				}}>
+				{data.map((user) => {
+					return (
+						<>
+							<Card
+								sx={{
+									width: "200px",
+									height: "300px",
+									marginBottom: "20px"
+								}}>
+								<CardActionArea>
+									<CardMedia
+										component="img"
+										height="140"
+										image={user.picture.large}
+									/>
+									<CardContent
+										sx={{
+											display: "flex",
+											justifyContent: "center"
+										}}>
+										<Typography variant="h6" component="div">
+											{user.name.first} {user.name.last}
+										</Typography>
+									</CardContent>
+								</CardActionArea>
+								<CardActions
+									sx={{ display: "flex", justifyContent: "end" }}>
+									<Button size="medium">
+										<DeleteIcon />
+									</Button>
+								</CardActions>
+							</Card>
+						</>
+					);
+				})}
+			</Box>
+		</>
 	);
 };
 
